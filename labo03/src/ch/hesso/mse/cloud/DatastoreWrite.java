c715a16f14981fd0d35e79df62ddf3901e84aa0c
Michael Caraccio <michael.caraccio@he-arc.ch>
2015-11-11 23:39:25 +0100

    initial commit

---
+++ labo03/src/ch/hesso/mse/cloud/DatastoreWrite.java
@@ -0,0 +1,85 @@
+package ch.hesso.mse.cloud;
+
+import com.google.appengine.api.datastore.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import java.util.Map;
+
+@SuppressWarnings("serial")
+public class DatastoreWrite extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        resp.setContentType("text/plain");
+        PrintWriter pw = resp.getWriter();
+
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+        // Insert new entry in DataStore
+        NewEntry(req, resp, datastore);
+
+        // Display stored objects in DataStore
+        DisplayAllEntries(datastore, pw);
+    }
+
+    /**
+     * Display stored objects in Datastore
+     *
+     * @param datastore DatastoreService
+     * @param pw PrintWriter
+     */
+    public void DisplayAllEntries(DatastoreService datastore, PrintWriter pw) {
+
+        // Use PreparedQuery interface to retrieve results
+        PreparedQuery pq = datastore.prepare(new Query());
+
+        for (Entity result : pq.asIterable()) {
+            Map<String, Object> properties = result.getProperties();
+
+            //loop a Map
+            for (Map.Entry<String, Object> entry : properties.entrySet()) {
+                pw.println("Key : " + entry.getKey() + " Value : " + entry.getValue());
+            }
+        }
+    }
+
+    /**
+     * Insert new entry in DataStore
+     * @param req HttpServletRequest
+     * @param resp HttpServletResponse
+     * @param datastore DatastoreService
+     * @throws IOException
+     */
+    public void NewEntry(HttpServletRequest req, HttpServletResponse resp, DatastoreService datastore) throws IOException {
+
+        PrintWriter pw = resp.getWriter();
+
+        String pkey;
+        String pval;
+
+        Enumeration parameters = req.getParameterNames();
+
+        // Get every parameters and store them in datastore
+        if (req.getParameter("_kind") != null && req.getParameter("_key") != null) {
+
+            Entity object = new Entity(req.getParameter("_kind"), req.getParameter("_key"));
+            while (parameters.hasMoreElements()) {
+                pkey = (String) parameters.nextElement();
+                pval = req.getParameter(pkey);
+
+                object.setProperty(pkey, pval);
+            }
+            datastore.put(object);
+        } else {
+            pw.println("Error : Parameters _key or _kind not found");
+        }
+    }
+}
