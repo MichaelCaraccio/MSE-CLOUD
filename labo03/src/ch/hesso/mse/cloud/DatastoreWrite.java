587bc1da7d681f45680bdacfa62f935dc84c0e68
krypty <gmarigliano93@gmail.com>
2015-11-14 17:21:29 +0100

    ignore _kind and _key parameters when adding entity

--- labo03/src/ch/hesso/mse/cloud/DatastoreWrite.java
+++ labo03/src/ch/hesso/mse/cloud/DatastoreWrite.java
@@ -8,11 +8,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.Map;
 
 @SuppressWarnings("serial")
 public class DatastoreWrite extends HttpServlet {
+
+    private static List<String> EXCLUDED_PARAMETERS = Arrays.asList("_kind", "_key");
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -34,7 +38,7 @@ public class DatastoreWrite extends HttpServlet {
      * Display stored objects in Datastore
      *
      * @param datastore DatastoreService
-     * @param pw PrintWriter
+     * @param pw        PrintWriter
      */
     public void DisplayAllEntries(DatastoreService datastore, PrintWriter pw) {
 
@@ -53,8 +57,9 @@ public class DatastoreWrite extends HttpServlet {
 
     /**
      * Insert new entry in DataStore
-     * @param req HttpServletRequest
-     * @param resp HttpServletResponse
+     *
+     * @param req       HttpServletRequest
+     * @param resp      HttpServletResponse
      * @param datastore DatastoreService
      * @throws IOException
      */
@@ -71,17 +76,20 @@ public class DatastoreWrite extends HttpServlet {
         if (req.getParameter("_kind") != null) {
 
             // If kind and key set by user
-            if(req.getParameter("_key") != null){
+            if (req.getParameter("_key") != null) {
                 object = new Entity(req.getParameter("_kind"), req.getParameter("_key"));
             }
             // If key not set - Datastore create it automatically
-            else{
+            else {
                 object = new Entity(req.getParameter("_kind"));
             }
 
             while (parameters.hasMoreElements()) {
                 pkey = (String) parameters.nextElement();
                 pval = req.getParameter(pkey);
+
+                if(EXCLUDED_PARAMETERS.contains(pkey))
+                    continue;
 
                 object.setProperty(pkey, pval);
             }
